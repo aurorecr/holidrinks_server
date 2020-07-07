2f71f4e51a91ad2e6e5a31cb339d0ba19c6fea7d
Aurore CR <aurorenaturerouge@hotmail.fr>
2020-07-07 16:46:55 +0200

    make it better

--- app.js
+++ app.js
@@ -50,7 +50,7 @@ app.use((error, req, res, next) => {
 });
 
 mongoose
-  .connect('mongodb+srv://aurorecr:aurorecr@cluster0-danc1.mongodb.net/mern?retryWrites=true&w=majority')
+  .connect('mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0-danc1.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority')
   .then(() => {
     console.log('DB Connected')
     app.listen(5000);
--- util/location.js
+++ util/location.js
@@ -2,7 +2,7 @@ const axios = require('axios');
 
 const HttpError = require('../models/http-error');
 
-const API_KEY = 'AIzaSyDUa8aXuPdbFE2OzW_uJqXkeq4HtyHNxt8';
+const API_KEY = process.env.GOOGLE_API_KEY;
 
 async function getCoordsForAddress(address) {
 
