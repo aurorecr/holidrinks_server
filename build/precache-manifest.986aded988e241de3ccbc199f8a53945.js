97bbfbaa4222762de8ee6a104dcab2ef3453287e
Aurore CR <aurorenaturerouge@hotmail.fr>
2020-07-07 20:26:35 +0200

    build

---
+++ build/precache-manifest.986aded988e241de3ccbc199f8a53945.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c14d918d4708ee53ee14d062a06fa16f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ece7d82ce58e56609eff",
+    "url": "/static/css/main.f2093d0f.chunk.css"
+  },
+  {
+    "revision": "7e433db61fb869988c7c",
+    "url": "/static/js/2.d0e98604.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.d0e98604.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ece7d82ce58e56609eff",
+    "url": "/static/js/main.5b252a02.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
